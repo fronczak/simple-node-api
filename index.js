7bf26895009dcdb2df1e1b2b4b7d1e30d220c24b
Sylvia Fronczak <sylvia@sylvias-mbp.lan>
2020-06-07 12:09:23 -0400

    add unit tests

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express        = require('express');
 const port = 8000;
 const app            = express();
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
 app.get('/', (req, res) => {
-    res.json({message: 'Hello World!'});
+    res.send('Hello World!');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!');});
 
+module.exports = app
